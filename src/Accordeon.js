814b69a98606e91fc9cb7e900322d6c049911264
vansky17 <ivan.stefanov@gmx.de>
2019-10-19 21:19:43 +0200

    first commit

---
+++ src/Accordeon.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import './Accordeon.css'
+
+
+export default class Accordeon extends Component {
+  static defaultProps = {
+    sections: []
+  };
+
+  state = {
+    activeSectionIndex: null,
+  }
+
+  handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+  }
+
+  renderItem(section, idx, activeSectionIndex) {
+    return (
+      <li className='Accordion__item' key={idx}>
+        <button
+          type='button'
+          onClick={() => this.handleSetActiveSecion(idx)}
+        >
+          {section.title}
+        </button>
+        {(activeSectionIndex === idx) && <p>{section.content}</p>}
+      </li>
+    )
+  }
+
+  render() {
+    const { activeSectionIndex } = this.state
+    const { sections } = this.props
+    return (
+      <ul className='Accordeon'>
+        {sections.map((section, idx) =>
+          this.renderItem(section, idx, activeSectionIndex)
+        )}
+      </ul>
+    )
+  }
+}
